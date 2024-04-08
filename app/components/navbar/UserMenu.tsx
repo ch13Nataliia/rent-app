339df8d846ec466c3f4d6b557ed5a4c7ea75397f
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-04-08 11:47:29 +0100

    created registerModal hook

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,8 +3,10 @@ import { useCallback, useState } from 'react';
 import { IoMdMenu } from 'react-icons/io';
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -33,8 +35,8 @@ const UserMenu = () => {
       {isOpen && (
         <div className="absolute rounede-xl shadow-md w-[40vw] md:w-3/4 bg-white iverflow-hidden right-0 top-12 text-sm">
           <div className="flex flex-col cursor-pointer">
-            <MenuItem onClick={() => {}} label='Login'/>
-            <MenuItem onClick={() => {}} label='Sign up'/>
+            <MenuItem onClick={() => {}} label="Login" />
+            <MenuItem onClick={registerModal.onOpen} label="Sign up" />
           </div>
         </div>
       )}
