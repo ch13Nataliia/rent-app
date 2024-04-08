bc19d20040d7c8d7e5db9c307a6ef6490ef72627
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-04-08 11:04:53 +0100

    created ui modal for log in

---
+++ app/components/Button.tsx
@@ -0,0 +1,9 @@
+
+
+
+
+const Button = () => {
+  return ( <button>press</button> );
+}
+ 
+export default Button;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Nunito } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar/Navbar';
+import Modal from './components/modals/Modal';
 
 export const metadata = {
   title: 'Rent App',
@@ -20,6 +21,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal title='Hello World' isOpen/>
         <Navbar />
         {children}
       </body>
